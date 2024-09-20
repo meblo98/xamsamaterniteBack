ffd3338b4a358758eea7387c6796725ba644c4a1
meblo98 <lomouhamedelbachir@gmail.com>
2024-09-20 12:04:23 +0000

    gestion des visites

--- app/Http/Controllers/VisiteController.php
+++ app/Http/Controllers/VisiteController.php
@@ -5,15 +5,20 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreVisiteRequest;
 use App\Http\Requests\UpdateVisiteRequest;
 use App\Models\Visite;
+use Illuminate\Http\Response;
 
 class VisiteController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+    // Lister toutes les visites
     public function index()
     {
-        //
+        $visites = Visite::all();
+        return response()->json([
+            'Liste des visites' => $visites
+        ], Response::HTTP_OK);
     }
 
     /**
@@ -27,17 +32,22 @@ class VisiteController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreVisiteRequest $request)
-    {
-        //
-    }
+   // Créer une nouvelle visite
+   public function store(StoreVisiteRequest $request)
+   {
+
+       $visite = Visite::create($request->all());
+       return response()->json($visite, Response::HTTP_CREATED);
+   }
 
     /**
      * Display the specified resource.
      */
-    public function show(Visite $visite)
+    // Afficher une visite spécifique
+    public function show($id)
     {
-        //
+        $visite = Visite::findOrFail($id);
+        return response()->json($visite, Response::HTTP_OK);
     }
 
     /**
@@ -51,16 +61,28 @@ class VisiteController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateVisiteRequest $request, Visite $visite)
+   
+    // Mettre à jour une visite
+    public function update(UpdateVisiteRequest $request, $id)
     {
-        //
+        
+        $visite = Visite::findOrFail($id);
+        $visite->update($request->all());
+
+        return response()->json($visite, Response::HTTP_OK);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Visite $visite)
-    {
-        //
-    }
+   // Supprimer une visite
+   public function destroy($id)
+   {
+       $visite = Visite::findOrFail($id);
+       $visite->delete();
+
+       return response()->json([
+           'message' => 'Visite supprimée'
+       ], Response::HTTP_OK);
+   }
 }
--- app/Http/Requests/StoreVisiteRequest.php
+++ app/Http/Requests/StoreVisiteRequest.php
@@ -11,7 +11,7 @@ class StoreVisiteRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class StoreVisiteRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+           'libelle' => 'required|string|max:255',
         ];
     }
 }
